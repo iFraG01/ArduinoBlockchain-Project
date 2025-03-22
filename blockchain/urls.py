dc2c26d0730f65e11409a005d838576875d213c1
iFraG01 <francescogarofalo2001@gmail.com>
2025-03-22 16:38:58 +0100

    Correzione urls

--- blockchain/urls.py
+++ blockchain/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from . import views
 from django.shortcuts import redirect
 from .views import edit_user, get_access_logs_view, get_user_operations_view, receive_esp32_data, user_management, delete_user, add_user,custom_login, custom_logout
 
 urlpatterns = [
     path("login/", custom_login, name="login"),
-    path("receive-data/", views.receive_esp32_data, name="receive_esp32_data"),
+    path("receive-data/", receive_esp32_data, name="receive_esp32_data"),
     path('get_access_logs/', get_access_logs_view, name='get_access_logs'),
     path('get_user_operations/', get_user_operations_view, name='get_user_operations'),
     path('users/', user_management, name='user_management'),
